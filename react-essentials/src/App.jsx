8dc4ed6f61681112566de25a709b389ecfce2c33
amarchino <marchino.alessandro@gmail.com>
2025-04-21 18:52:33 +0200

    Lesson 52

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -4,8 +4,8 @@ import TabButton from './components/TabButton';
 import { CORE_CONCEPTS } from './data';
 
 function App() {
-  function handleClick() {
-    console.log('Hello world!');
+  function handleClick(selectedButton) {
+    console.log(selectedButton);
   }
 
   return (
@@ -24,10 +24,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
