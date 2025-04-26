47867c7623b766e110dda0def19d55b3d250f10a
amarchino <marchino.alessandro@gmail.com>
2025-04-26 14:27:31 +0200

    Lesson 67

--- react-essentials/src/components/Examples.jsx
+++ react-essentials/src/components/Examples.jsx
@@ -3,9 +3,11 @@ import { CORE_CONCEPTS, EXAMPLES } from '../data';
 import Example from './Example';
 import TabButton from './TabButton';
 import Section from './Section';
+import Tabs from './Tabs';
 
 export default function Examples() {
   const [ selectedTopic, setSelectedTopic ] = useState();
+  const tabContent = !selectedTopic ? <p>Please select a topic</p> : <Example { ...EXAMPLES[selectedTopic] } /> ;
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -13,10 +15,9 @@ export default function Examples() {
 
   return (
     <Section id="examples" title="Examples">
-      <menu>
-        { CORE_CONCEPTS.map(el => <TabButton key={el.title} isSelected={selectedTopic === el.title.toLowerCase()} onClick={() => handleClick(el.title.toLowerCase())}>{ el.title }</TabButton>) }
-      </menu>
-      { !selectedTopic ? <p>Please select a topic</p> : <Example { ...EXAMPLES[selectedTopic] } /> }
+      <Tabs buttons={ CORE_CONCEPTS.map(el => <TabButton key={el.title} isSelected={selectedTopic === el.title.toLowerCase()} onClick={() => handleClick(el.title.toLowerCase())}>{ el.title }</TabButton>) }>
+        { tabContent }
+      </Tabs>
     </Section>
   );
 }
