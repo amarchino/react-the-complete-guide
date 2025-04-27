4d6629ca9801f5505828a39f8fe044ef2a2e9714
amarchino <marchino.alessandro@gmail.com>
2025-04-27 15:29:28 +0200

    Lesson 85

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,11 +1,22 @@
-export default function GameBoard({ onSelectSquare }) {
+const initialGameBoard = [
+  [ null, null, null ],
+  [ null, null, null ],
+  [ null, null, null ]
+];
+
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = [ ...initialGameBoard.map(row => [ ...row ]) ];
+
+  for(const { square: { row, col }, player } of turns) {
+    gameBoard[row][col] = player;
+  }
 
   return (
     <ol id="game-board">
       { gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
           { row.map((playerSymbol, columnIndex) => <li key={columnIndex}>
-            <button onClick={onSelectSquare}>{ playerSymbol }</button>
+            <button onClick={ () => onSelectSquare(rowIndex, columnIndex) }>{ playerSymbol }</button>
           </li>) }
         </ol>
       </li>) }
