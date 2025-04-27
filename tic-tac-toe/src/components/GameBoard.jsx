466fe014e64f5afb727f966d594d28192a663985
amarchino <marchino.alessandro@gmail.com>
2025-04-27 14:58:14 +0200

    Lesson 81

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,12 +7,22 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex, symbol) {
+    setGameBoard(prevGameBoard => {
+      const updatedBoard = [...prevGameBoard.map(innerArray => [ ...innerArray ])];
+      updatedBoard[rowIndex][colIndex] = symbol;
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      { initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+      { gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
           { row.map((playerSymbol, columnIndex) => <li key={columnIndex}>
-            <button>{ playerSymbol }</button>
+            <button onClick={() => handleSelectSquare(rowIndex, columnIndex, 'X')}>{ playerSymbol }</button>
           </li>) }
         </ol>
       </li>) }
