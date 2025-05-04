60ca52e28e7a84cc00ecdd804c4326eeae9fc497
amarchino <marchino.alessandro@gmail.com>
2025-05-04 21:24:55 +0200

    Lesson 142

--- refs-portals/src/components/ResultModal.jsx
+++ refs-portals/src/components/ResultModal.jsx
@@ -12,8 +12,17 @@
 // })
 // export default ResultModal;
 
-export default function ResultModal({ ref, result , targetTime }) {
-  return (<dialog ref={ref} className="result-modal">
+import { useImperativeHandle, useRef } from 'react';
+
+export default function ResultModal({ ref, result, targetTime }) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => ({
+    open() {
+      dialog.current.showModal()
+    }
+  }));
+
+  return (<dialog ref={dialog} className="result-modal">
     <h2>You { result }</h2>
     <p>The target time was <strong>{targetTime} second{targetTime > 1 ? 's' : ''}</strong></p>
     <p>You stopped the timer with <strong>X seconds left</strong></p>
