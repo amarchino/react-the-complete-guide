bd386a95a7901c0e5e85f6704ff471f81173a95b
amarchino <marchino.alessandro@gmail.com>
2025-05-08 20:42:21 +0200

    Lesson 159

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   const [ projectsState, setProjectsState ] = useState({
@@ -32,8 +33,15 @@ function App() {
       selectedProjectId: undefined
     }));
   }
+  function handleSelectProject(id) {
+    setProjectsState(prevState => ({
+      ...prevState,
+      selectedProjectId: id
+    }))
+  }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+  let content = <SelectedProject project={selectedProject} />;
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={addProject} onCancel={handleCancelAddProject} />
   } else if(projectsState.selectedProjectId === undefined) {
@@ -42,7 +50,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={ projectsState.projects } />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={ projectsState.projects } onSelectProject={handleSelectProject} selectedProjectId={projectsState.selectedProjectId} />
       { content }
     </main>
   );
