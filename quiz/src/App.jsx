f3b7ee4841744464d40416258c3ecea0f7c56f22
amarchino <marchino.alessandro@gmail.com>
2025-05-11 16:50:24 +0200

    Lesson 195

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,7 +1,13 @@
 import Header from './components/Header';
+import Quiz from './components/Quiz';
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
   );
 }
--- quiz/src/components/Quiz.jsx
+++ quiz/src/components/Quiz.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions';
+
 export default function Quiz() {
-  const [ activeQuestionIndex, setActiveQuestionIndex ] = useState(0);
   const [ userAnswers, setUserAnswers ] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers(userAnswers => [ ...userAnswers, selectedAnswer ]);
+  }
 
   return (
-    <p>Currently active Question</p>
+    <div id="quiz">
+      <div id="question">
+        <h2>{ QUESTIONS[activeQuestionIndex].text }</h2>
+        <ul id="answers">
+          { QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer} className='answer'>
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>) }
+        </ul>
+      </div>
+
+    </div>
   );
 }
