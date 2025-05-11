64a93f00f1b59d26b59f5270d80ecde5d129155b
amarchino <marchino.alessandro@gmail.com>
2025-05-11 13:01:36 +0200

    Lesson 191

--- effects/src/components/DeleteConfirmation.jsx
+++ effects/src/components/DeleteConfirmation.jsx
@@ -1,8 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [ remainingTime, setRemainingTime ] = useState(TIMER);
+
   useEffect(() => {
-    const timer = setTimeout(onConfirm, 3000);
+    const timer = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 10);
+    }, 10);
+    return () => clearInterval(timer);
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(onConfirm, TIMER);
     return () => clearTimeout(timer);
   }, [ onConfirm ]);
 
@@ -11,13 +22,10 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
       <h2>Are you sure?</h2>
       <p>Do you really want to remove this place?</p>
       <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text">
-          No
-        </button>
-        <button onClick={onConfirm} className="button">
-          Yes
-        </button>
+        <button onClick={onCancel} className="button-text">No</button>
+        <button onClick={onConfirm} className="button">Yes</button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
