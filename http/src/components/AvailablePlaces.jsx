137e4a7ff6e2ee7d54b730a8139b42b360b48c34
amarchino <marchino.alessandro@gmail.com>
2025-05-14 21:36:00 +0200

    Lesson 237

--- http/src/components/AvailablePlaces.jsx
+++ http/src/components/AvailablePlaces.jsx
@@ -1,20 +1,34 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import ErrorPage from './ErrorPage.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
   const [ isFetching, setIsFetching ] = useState(false);
+  const [ error, setError ] = useState(null);
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        if(!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+        const resData = await response.json();
+        setAvailablePlaces(resData.places);
+      } catch(error) {
+        setError({ message: error.message || 'Could not fetch places, please try again later.' });
+      }
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if(error) {
+    return <ErrorPage title="An error occurred!" message={error.message} />
+  }
+
   return (
     <Places
       title="Available Places"
