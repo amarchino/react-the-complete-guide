19cd58018d8f0541296824ef2f0dfa6f837ae4ed
amarchino <marchino.alessandro@gmail.com>
2025-05-14 21:29:06 +0200

    Lesson 235

--- http/src/components/AvailablePlaces.jsx
+++ http/src/components/AvailablePlaces.jsx
@@ -3,14 +3,23 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
+  const [ isFetching, setIsFetching ] = useState(false);
+
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then(response => response.json())
-      .then(resData => setAvailablePlaces(resData.places));
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+    fetchPlaces();
   }, []);
   return (
     <Places
       title="Available Places"
+      isLoading={isFetching}
+      loadingText={"Fetching place data..."}
       places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
--- http/src/components/Places.jsx
+++ http/src/components/Places.jsx
@@ -1,10 +1,11 @@
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({ title, places, fallbackText, onSelectPlace, isLoading, loadingText }) {
   console.log(places);
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
+      { isLoading && <p class="fallback-text">{loadingText}</p> }
+      { !isLoading && places.length === 0 && <p className="fallback-text">{fallbackText}</p> }
+      { !isLoading && places.length > 0 && (
         <ul className="places">
           {places.map((place) => (
             <li key={place.id} className="place-item">
@@ -15,7 +16,7 @@ export default function Places({ title, places, fallbackText, onSelectPlace }) {
             </li>
           ))}
         </ul>
-      )}
+      ) }
     </section>
   );
 }
