fc728feb1aef394287caeccbb94e4f37a0a2bd1c
amarchino <marchino.alessandro@gmail.com>
2025-05-18 21:06:24 +0200

    Lesson 270

--- form-action/src/components/Signup.jsx
+++ form-action/src/components/Signup.jsx
@@ -1,6 +1,8 @@
+import { useActionState } from 'react';
 import { hasMinLength, isEmail, isEqualToOtherValue, isNotEmpty } from '../util/validation';
+
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -15,7 +17,7 @@ export default function Signup() {
       errors.push('Invalid email address.');
     }
     if(!isNotEmpty(password) || !hasMinLength(password, 6)) {
-      error.push('You must provide a password with at least six characters.');
+      errors.push('You must provide a password with at least six characters.');
     }
     if(!isEqualToOtherValue(password, confirmPassword)) {
       errors.push('Passwords do not match.');
@@ -32,10 +34,16 @@ export default function Signup() {
     if(acquisitionChannel.length === 0) {
       errors.push('Please select at least one acquisition channel.')
     }
+    if(errors.length > 0) {
+      return { errors };
+    }
+    return { errors: null };
   }
+  const [ formState, formAction ] = useActionState(signupAction, { errors: null });
+
 
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -119,6 +127,10 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      { formState.errors && <ul className="error">
+        { formState.errors.map(error => <li key={error}>{ error }</li>)}
+        </ul>}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
