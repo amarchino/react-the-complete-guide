6f3c69439800358e5f4ab292fc00acc19cf35688
amarchino <marchino.alessandro@gmail.com>
2025-05-22 21:55:51 +0200

    Lesson 322

--- redux-basic/src/App.jsx
+++ redux-basic/src/App.jsx
@@ -1,12 +1,16 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuthenticated = useSelector(store => store.auth.isAuthenticated);
+
   return (
     <>
       <Header />
-      <Auth />
+      { !isAuthenticated ? <Auth /> : <UserProfile /> }
       <Counter />
     </>
   );
