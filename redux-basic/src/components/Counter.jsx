9b091d7bafda28294033a63ccd00fc599ddadd25
amarchino <marchino.alessandro@gmail.com>
2025-05-22 20:57:53 +0200

    Lesson 315

--- redux-basic/src/components/Counter.jsx
+++ redux-basic/src/components/Counter.jsx
@@ -3,18 +3,19 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => dispatch({ type: 'increment' });
   const decrementHandler = () => dispatch({ type: 'decrement' });
   const increaseHandler = () => dispatch({ type: 'increase', amount: 5 });
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => { dispatch({ type: 'toggle' })};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{ counter }</div>
+      { showCounter && <div className={classes.value}>{ counter }</div> }
       <div >
         <button onClick={ incrementHandler }>Increment</button>
         <button onClick={ increaseHandler }>Increase by 5</button>
