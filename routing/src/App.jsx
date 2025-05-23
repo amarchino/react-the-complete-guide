0c4bdf03e3c3670940de7839844641a1b40c1965
amarchino <marchino.alessandro@gmail.com>
2025-05-23 23:30:38 +0200

    Lesson 347

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,19 +1,11 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-const router = createBrowserRouter(routeDefinition);
-
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage /> },
-//   { path: '/products', element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
