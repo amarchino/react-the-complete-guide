ba7b926e12bb46ad601caf1c0f44e4a6b8620c8e
amarchino <marchino.alessandro@gmail.com>
2025-05-23 15:00:00 +0200

    Lesson 333

--- redux-advanced/src/App.jsx
+++ redux-advanced/src/App.jsx
@@ -2,9 +2,21 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-redux-learn-fd7e3-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cart)
+    });
+  }, [ cart ]);
 
   return (
     <Layout>
