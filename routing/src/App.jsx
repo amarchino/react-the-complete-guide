c9423edaf77b7e787690b5f40ff45c2940effe85
amarchino <marchino.alessandro@gmail.com>
2025-05-23 21:19:25 +0200

    Lesson 345

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> }
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
 ]);
 
 function App() {
