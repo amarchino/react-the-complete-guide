cb7af5a41c44fb6fe538a5ee8c5406e069d7358a
amarchino <marchino.alessandro@gmail.com>
2025-05-25 12:35:10 +0200

    Lesson 352

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 import RootLayout from './pages/RootLayout';
 import ErrorPage from './pages/ErrorPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
     ]
   }
 ]);
