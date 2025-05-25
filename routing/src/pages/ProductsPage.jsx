613052ff2aa25bdf3fa3e8bf2fe444b618365c69
amarchino <marchino.alessandro@gmail.com>
2025-05-25 12:39:46 +0200

    Lesson 353

--- routing/src/pages/ProductsPage.jsx
+++ routing/src/pages/ProductsPage.jsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const PRODUCTS = [
+  { id: 'p1', title: 'Product 1'},
+  { id: 'p2', title: 'Product 2'},
+  { id: 'p3', title: 'Product 3'}
+];
+
+
 export default function ProductsPage() {
   return <>
     <h1>The Products Page</h1>
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      { PRODUCTS.map(p => <li key={p.id}>
+        <Link to={`/products/${p.id}`}>{ p.title }</Link>
+      </li>)  }
     </ul>
   </>
 }
