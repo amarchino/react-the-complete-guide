a4ce7be639763de72ee64d87bad946d173ca8882
amarchino <marchino.alessandro@gmail.com>
2025-05-26 17:03:46 +0200

    Lesson 377

--- routing-advanced/frontend/src/App.jsx
+++ routing-advanced/frontend/src/App.jsx
@@ -1,22 +1,23 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
-import NewEventPage, { action as newEventAction} from './pages/NewEventPage';
+import NewEventPage from './pages/NewEventPage';
 import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetailPage';
 import EditEventPage from './pages/EditEventPage';
 import RootLayout from './pages/RootLayout';
 import EventsRootLayout from './pages/EventsRootLayout';
 import ErrorPage from './pages/ErrorPage';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
   { path: '/', element: <RootLayout />, errorElement: <ErrorPage />, children: [
     { index: true, element: <HomePage /> },
     { path: 'events', element: <EventsRootLayout />, children: [
       { index: true, element: <EventsPage />, loader: eventsLoader },
-      { path: 'new', element: <NewEventPage />, action: newEventAction },
+      { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
       { path: ':eventId', id: 'event-detail', loader: eventDetailLoader, children: [
         { index: true, element: <EventDetailPage />, action: deleteEventAction },
-        { path: 'edit', element: <EditEventPage /> }
+        { path: 'edit', element: <EditEventPage />, action: manipulateEventAction }
       ]}
     ]}
   ] }
--- routing-advanced/frontend/src/components/EventForm.jsx
+++ routing-advanced/frontend/src/components/EventForm.jsx
@@ -1,4 +1,4 @@
-import { Form, useActionData, useNavigate, useNavigation } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
@@ -12,7 +12,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method='post' className={classes.form}>
+    <Form method={method} className={classes.form}>
       { data && data.errors && <ul>
         { Object.values(data.errors).map(err => <li key={err}>{ err }</li>) }
       </ul>}
@@ -41,3 +41,33 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description')
+  };
+  let url = 'http://localhost:8080/events';
+  if(request.method === 'PATCH') {
+    const eventId = params.eventId;
+    url = `http://localhost:8080/events/${eventId}`;
+  }
+  const response = await fetch(url, {
+    method: request.method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(eventData)
+  });
+  if(response.status === 422) {
+    return response;
+  }
+  if(!response.ok) {
+    throw new Response({ message: 'Could not save event'}, { status: 500 });
+  }
+  return redirect('/events');
+}
