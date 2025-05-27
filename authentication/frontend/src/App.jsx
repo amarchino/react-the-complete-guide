f9261356342439e1772d2c9514f5693ea0304cf2
amarchino <marchino.alessandro@gmail.com>
2025-05-27 20:56:59 +0200

    Lesson 389

--- authentication/frontend/src/App.jsx
+++ authentication/frontend/src/App.jsx
@@ -10,7 +10,7 @@ import NewEventPage from './pages/NewEventPage';
 import RootLayout from './pages/RootLayout';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/NewsletterPage';
-import AuthenticationPage from './pages/AuthenticationPage';
+import AuthenticationPage, { action as authAction } from './pages/AuthenticationPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <RootLayout />, errorElement: <ErrorPage />, children: [
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       { path: 'new', element: <NewEventPage />, action: manipulateEventAction, }
     ]},
     { path: 'newsletter', element: <NewsletterPage />, action: newsletterAction },
-    { path: 'auth', element: <AuthenticationPage />  },
+    { path: 'auth', element: <AuthenticationPage />, action: authAction  }
   ]},
 ]);
 
