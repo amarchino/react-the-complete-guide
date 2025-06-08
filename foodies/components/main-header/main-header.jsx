64c4c9a33ee7432b33869ff68ea3752a2734c182
amarchino <marchino.alessandro@gmail.com>
2025-06-08 19:43:06 +0200

    Lesson 447

--- foodies/components/main-header/main-header.jsx
+++ foodies/components/main-header/main-header.jsx
@@ -4,6 +4,7 @@ import classes from './main-header.module.css';
 import logoImg from '@/assets/logo.png'
 import Image from 'next/image';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
   return (
@@ -16,8 +17,8 @@ export default function MainHeader() {
         </Link>
         <nav className={classes.nav}>
           <ul>
-            <li><Link href="/meals">Browse Meals</Link></li>
-            <li><Link href="/community">Foodies Community</Link></li>
+            <li><NavLink href="/meals">Browse Meals</NavLink></li>
+            <li><NavLink href="/community">Foodies Community</NavLink></li>
           </ul>
         </nav>
       </header>
