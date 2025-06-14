ba579687d1ee2a1d5ec62cbe95f104bd2405fc15
amarchino <marchino.alessandro@gmail.com>
2025-06-14 21:13:09 +0200

    Lesson 489

--- nextjs-meetup/pages/[meetupId]/index.jsx
+++ nextjs-meetup/pages/[meetupId]/index.jsx
@@ -1,3 +1,11 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
 export default function MeetupDetailPage() {
-  return <h1>Meetup Detail</h1>;
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      description="This is a first meetup"
+      address="Some address 5, 12345 Some City" />
+  );
 }
