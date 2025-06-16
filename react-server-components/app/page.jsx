85966d4455d31b3d99b64f99a9cab862f4cd1201
amarchino <marchino.alessandro@gmail.com>
2025-06-16 21:24:48 +0200

    Lesson 513

--- react-server-components/app/page.jsx
+++ react-server-components/app/page.jsx
@@ -2,14 +2,19 @@ import ClientDemo from '@/components/ClientDemo';
 import DataFetchingDemo from '@/components/DataFetchingDemo';
 import RSCDemo from '@/components/RCSDemo';
 import ServerActionsDemo from '@/components/ServerActionsDemo';
+import UsePromiseDemo from '@/components/UsePromisesDemo';
+import { Suspense } from 'react';
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
       <RSCDemo />
       <ClientDemo />
       <DataFetchingDemo />
       <ServerActionsDemo />
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UsePromiseDemo />
+      </Suspense>
     </main>
   );
 }
