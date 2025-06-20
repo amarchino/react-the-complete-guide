b8c2034e0630aa79f8a3121c748e71c198d7ef9f
amarchino <marchino.alessandro@gmail.com>
2025-06-20 22:27:58 +0200

    Lesson 527

--- animations/src/components/Modal.jsx
+++ animations/src/components/Modal.jsx
@@ -2,10 +2,14 @@ import { createPortal } from 'react-dom';
 import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
+  const variant = {
+    hidden: { opacity: 0, y: 30 },
+    visible: { opacity: 1, y: 0 }
+  }
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <motion.dialog open className="modal" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 30 }} >
+      <motion.dialog open className="modal" variants={variant} initial="hidden" animate="visible" exit="hidden" >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
