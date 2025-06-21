fe8189dd7a18e533b2ef7981fae64f5053a676ab
amarchino <marchino.alessandro@gmail.com>
2025-06-21 19:48:13 +0200

    Lesson 541

--- patterns/src/components/accordion/Accordion.jsx
+++ patterns/src/components/accordion/Accordion.jsx
@@ -1,7 +1,38 @@
+import { createContext, useContext, useState } from 'react';
+
+const AccordionContext = createContext({
+  openItemId: 1,
+  openItem: (id) => {},
+  closeItem: () => {},
+});
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+  if(!ctx) {
+    throw new Error('Accordion-related componentes must be wrapper by <Accordion>');
+  }
+  return ctx;
+}
+
 export default function Accordion({ children, className }) {
+  const [ openItemId, setOpenItemId ] = useState();
+  function openItem(id) {
+    setOpenItemId(id)
+  }
+  function closeItem() {
+    setOpenItemId(null)
+  }
+  const contextValue = {
+    openItemId,
+    openItem,
+    closeItem
+  };
+
   return (
-    <ul className={className}>
-      { children }
-    </ul>
+    <AccordionContext value={contextValue}>
+      <ul className={className}>
+        { children }
+      </ul>
+    </AccordionContext>
   );
 }
