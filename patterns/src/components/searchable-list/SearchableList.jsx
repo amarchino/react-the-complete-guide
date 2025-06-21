8a7e0e10b8595d338380951c3fe67b7ce781767e
amarchino <marchino.alessandro@gmail.com>
2025-06-21 21:31:36 +0200

    Lesson 549

--- patterns/src/components/searchable-list/SearchableList.jsx
+++ patterns/src/components/searchable-list/SearchableList.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export default function SearchableList({ items, itemKeyFn = item => item, children }) {
   const [ searchTerm, setSearchTerm ] = useState('');
+  const lastChange = useRef();
 
   const searchResults = items.filter(item => JSON.stringify(item).toLowerCase().includes(searchTerm));
 
   function handleChange(event) {
-    setSearchTerm(event.target.value.toLowerCase());
+    if(lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      setSearchTerm(event.target.value.toLowerCase());
+    }, 500)
   }
 
   return <div className='searchable-list'>
