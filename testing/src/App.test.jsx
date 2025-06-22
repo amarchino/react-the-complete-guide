694bf72a3440a525f317409033c82cb6525c60e7
amarchino <marchino.alessandro@gmail.com>
2025-06-22 22:13:59 +0200

    Lesson 570

--- testing/src/App.test.jsx
+++ testing/src/App.test.jsx
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it } from 'vitest';
 import App from './App';
+import { cleanup, render, screen } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  afterEach(() => {
+    cleanup();
+  })
+
+  it('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeDefined();
+  });
+
 });
