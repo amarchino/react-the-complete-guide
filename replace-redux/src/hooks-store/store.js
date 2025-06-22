927d4390b55e1a408648585e1f4663601bcd2f4a
amarchino <marchino.alessandro@gmail.com>
2025-06-22 20:09:57 +0200

    Lesson 557

---
+++ replace-redux/src/hooks-store/store.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export default function useStore() {
+  const [ _, setState ] = useState(globalState);
+
+  function dispatch(actionIdentifier) {
+    const newState = actions[actionIdentifier]?.(globalState);
+    globalState = { ...globalState, ...newState };
+    listeners.forEach(li => li(globalState));
+  }
+
+  useEffect(() => {
+    listeners.push(setState);
+    return () => {
+      listeners = listeners.filte(li => li !== setState);
+    }
+  }, [ setState ]);
+
+  return [ globalState, dispatch ];
+}
+
+export function initStore(userActions, initialState) {
+  if(initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+
+}
