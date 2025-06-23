6033ed82df09854c2af403d0f17fa90867ccb63c
amarchino <marchino.alessandro@gmail.com>
2025-06-23 22:54:08 +0200

    Lesson 573

--- testing/src/components/Greeting.test.jsx
+++ testing/src/components/Greeting.test.jsx
@@ -1,4 +1,5 @@
 import { cleanup, render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { afterEach, describe, expect, test } from 'vitest';
 import Greeting from './Greeting';
 
@@ -14,6 +15,43 @@ describe('<Greeting />', () => {
     // Assert
     const helloWorldElement = screen.queryByText('Hello World!');
     expect(helloWorldElement).not.toBeNull();
+  });
+
+  test('Renders "good to see you" if the button was NOT clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+
+    // Assert
+    const paragraph = screen.queryByText('good to see you', { exact: false });
+    expect(paragraph).not.toBeNull();
+  });
+
+  test('Renders "Changed!" if the button was clicked', async () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.queryByRole('button');
+    await userEvent.click(buttonElement);
+
+    // Assert
+    const paragraph = screen.queryByText('Changed!');
+    expect(paragraph).not.toBeNull();
+  });
+
+  test('Does not render "good to see you!" if the button was clicked', async () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.queryByRole('button');
+    await userEvent.click(buttonElement);
+
+    // Assert
+    const paragraph = screen.queryByText('good to see you', { exact: false });;
+    expect(paragraph).toBeNull();
   });
 
 })
