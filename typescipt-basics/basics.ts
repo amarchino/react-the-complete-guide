aca7d124a072bad4a2d6db9dbbe54cf144297ffa
amarchino <marchino.alessandro@gmail.com>
2025-06-24 19:44:07 +0200

    Lesson 587

--- typescipt-basics/basics.ts
+++ typescipt-basics/basics.ts
@@ -31,3 +31,11 @@ function add(a: number, b: number): number {
 function printOutput(value: any): void {
     console.log(value)
 }
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T): T[] {
+    return [ value, ...array ]
+}
+const demoArray = [ 1, 2, 3 ];
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning([ 'a', 'b', 'c' ], 'd');
