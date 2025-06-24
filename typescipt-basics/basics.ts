54cc377cbd533c89da9dd42283a6dacb346f9cee
amarchino <marchino.alessandro@gmail.com>
2025-06-24 19:35:10 +0200

    Lesson 584

--- typescipt-basics/basics.ts
+++ typescipt-basics/basics.ts
@@ -16,3 +16,7 @@ let person: { name: string, age: number };
 person = { name: 'Max', age: 32 };
 
 let people: { name: string, age: number }[];
+
+// Union types
+let course: string | number = 'React - The Complete Guide';
+course = 12341;
