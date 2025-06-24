f194e1e4c51be730f1743f88f9504e6f19fa4482
amarchino <marchino.alessandro@gmail.com>
2025-06-24 23:19:28 +0200

    Lesson 593

--- typescript/src/components/Todos.tsx
+++ typescript/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import type { FC } from 'react';
 import type { Todo } from '../models/Todo';
+import TodoItem from './TodoItem';
 
 const Todos: FC<{ items: Todo[] }> = ({ items }) => {
   return (
     <ul>
-      { items.map(item => <li key={ item.id }>{ item.text }</li>) }
+      { items.map(item => <TodoItem key={ item.id } text={ item.text } />) }
     </ul>
   );
 }
