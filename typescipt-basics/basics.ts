dd3e7dfe764765432d6fa7853095d86967dd195b
amarchino <marchino.alessandro@gmail.com>
2025-06-24 19:33:10 +0200

    Lesson 582

--- typescipt-basics/basics.ts
+++ typescipt-basics/basics.ts
@@ -7,3 +7,12 @@ userName = 'Max';
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// Arrays and Objects
+let hobbies: string[];
+hobbies = [ 'Sports', 'Cooking' ];
+
+let person: { name: string, age: number };
+person = { name: 'Max', age: 32 };
+
+let people: { name: string, age: number }[];
