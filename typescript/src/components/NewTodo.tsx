a049cbd9d8690e3295bb6fe96a1752a4ab0d41d4
amarchino <marchino.alessandro@gmail.com>
2025-06-25 22:06:41 +0200

    Lesson 595

--- typescript/src/components/NewTodo.tsx
+++ typescript/src/components/NewTodo.tsx
@@ -1,14 +1,21 @@
-import type { FC, FormEvent } from 'react';
+import { useRef, type FC, type FormEvent } from 'react';
 
 const NewTodo: FC = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   function submitHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    const enteredText = todoTextInputRef.current!.value.trim();
+    if(enteredText.length === 0) {
+      return;
+    }
+
   }
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add todo</button>
     </form>
   )
