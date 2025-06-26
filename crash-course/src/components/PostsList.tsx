922c58355c6408912675e441e94cf3ade9f47f2f
amarchino <marchino.alessandro@gmail.com>
2025-06-26 22:49:27 +0200

    Lesson 615

--- crash-course/src/components/PostsList.tsx
+++ crash-course/src/components/PostsList.tsx
@@ -1,13 +1,17 @@
 import type { FC } from 'react';
 import Post from './Post';
 import classes from './PostsList.module.css'
+import NewPost from './NewPost';
 
 const PostsList: FC = () => {
   return (
-    <ul className={classes.posts}>
-      <Post author='Maximilian' body='React.js is awesome!' />
-      <Post author='Manuel' body='Check out the full course!' />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author='Maximilian' body='React.js is awesome!' />
+        <Post author='Manuel' body='Check out the full course!' />
+      </ul>
+    </>
   );
 }
 
